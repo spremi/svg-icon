9f2d293260d1876ece44b575639a3547587d82a4
Sanjeev Premi <spremi@ymail.com>
2018-11-18 03:43:35 +0530

    feat: lib: svg-icon(mod): Prevent re-import
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- projects/spremi/svg-icon/src/lib/svg-icon.module.ts
+++ projects/spremi/svg-icon/src/lib/svg-icon.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
 import { SvgIconComponent } from './svg-icon.component';
 
 @NgModule({
-  imports: [
-  ],
+  imports: [],
   declarations: [SvgIconComponent],
-  exports: [SvgIconComponent]
+  exports: [SvgIconComponent],
 })
-export class SvgIconModule { }
+export class SvgIconModule {
+  constructor(@Optional() @SkipSelf() parentModule: SvgIconModule) {
+    if (parentModule) {
+      throw new Error(
+        'SvgIconModule is already loaded. Import it in the AppModule only',
+      );
+    }
+  }
+}
