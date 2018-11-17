b58fcc11d15f9b938a6e4f19eadaf6f0ec9b6886
Sanjeev Premi <spremi@ymail.com>
2018-11-18 03:38:40 +0530

    feat: lib: svg-icon(comp): Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- projects/spremi/svg-icon/src/lib/svg-icon.component.ts
+++ projects/spremi/svg-icon/src/lib/svg-icon.component.ts
@@ -1,19 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+
+import { SvgIconService } from './svg-icon.service';
+
+/**
+ * Default icon width
+ */
+const DefaultIconWidth = 24;
+
+/**
+ * Default icon height
+ */
+const DefaultIconHeight = 24;
 
 @Component({
   selector: 'ang-svg-icon',
   template: `
-    <p>
-      svg-icon works!
-    </p>
+    <span [style.fill]="err ? errFill : 'inherit'" [innerHtml]="markup"> </span>
   `,
-  styles: []
+  styles: [],
 })
-export class SvgIconComponent implements OnInit {
+export class SvgIconComponent implements OnInit, OnDestroy {
+  /**
+   * URL for the icon
+   */
+  private _url: string;
 
-  constructor() { }
+  /**
+   * Icon width
+   */
+  private _width: number;
 
-  ngOnInit() {
+  /**
+   * Icon height
+   */
+  private _height: number;
+
+  /**
+   * Subscription
+   */
+  private _sub: Subscription;
+
+  /**
+   * Sanitized SVG markup
+   */
+  public markup: SafeHtml;
+
+  /**
+   * Error fetching icon
+   */
+  public err = false;
+
+  /**
+   * Fill color for SVG icon
+   */
+  public errFill: string;
+
+  constructor(private iconSvc: SvgIconService) {}
+
+  @Input()
+  set url(url: string) {
+    this._url = url;
   }
 
+  @Input()
+  set width(width: string) {
+    this._width = this.toNumber(width, DefaultIconWidth);
+  }
+
+  @Input()
+  set height(height: string) {
+    this._height = this.toNumber(height, DefaultIconHeight);
+  }
+
+  ngOnInit() {
+    this.errFill = this.iconSvc.getErrorFill();
+
+    this.markup = this.iconSvc.getBlank(this._width, this._height);
+
+    this._sub = this.iconSvc.get(this._url).subscribe(
+      (icon) => {
+        this.markup = icon;
+      },
+      (error) => {
+        this.err = true;
+
+        this.markup = this.iconSvc.getError(this._width, this._height);
+      },
+    );
+  }
+
+  ngOnDestroy() {
+    if (this._sub) {
+      this._sub.unsubscribe();
+    }
+  }
+
+  /**
+   * Returns dimension as number.
+   * On error, returns fallback value.
+   */
+  private toNumber(arg: string, fallback: number): number {
+    const num = parseInt(arg, 10);
+
+    return isNaN(num) ? fallback : num;
+  }
 }
