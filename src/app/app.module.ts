02330ce26391e2562ccf9494c9cf875dda92ba14
Sanjeev Premi <spremi@ymail.com>
2018-11-19 00:24:11 +0530

    feat: app: Add test page for library '@spremi/svg-icon'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  SvgIconModule,
+  SvgIconService,
+} from 'projects/spremi/svg-icon/src/public_api';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, SvgIconModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private iconSvc: SvgIconService) {
+    this.iconSvc.setErrorFill('purple');
+  }
+}
