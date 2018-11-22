2f47e6ef70d3942277101b84533860a8c2ef15ec
Sanjeev Premi <spremi@ymail.com>
2018-11-23 01:45:29 +0530

    feat: app: Update the demo application
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'svg-icon';
+  title = 'Demo @spremi/svg-icon';
 }
