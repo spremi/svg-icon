6002d0424457a04464447ce82077be0b70c68b35
Sanjeev Premi <spremi@ymail.com>
2018-11-24 00:14:52 +0530

    feat: app: Set custom error SVG on startup
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,23 @@ import {
 
 import { AppComponent } from './app.component';
 
+/**
+ * Custom template for error SVG
+ */
+const CUST_ERROR = `<svg xmlns="http://www.w3.org/2000/svg"
+  width="%WIDTH%" height="%HEIGHT%" viewBox="0 0 24 24">
+    <path
+       d="M 8 8 L 8 16 L 16 16 L 16 8 L 8 8 z M 10 10 L 14 10 L 14 14 L 10 14 L 10 10 z " />
+    <rect
+       width="4" height="4" x="4" y="4" />
+    <rect
+       width="4" height="4" x="4" y="16" />
+    <rect
+       width="4" height="4" x="16" y="16" />
+    <rect
+       width="4" height="4" x="16" y="4" />
+  </svg >`;
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, SvgIconModule.forRoot()],
@@ -16,6 +33,7 @@ import { AppComponent } from './app.component';
 })
 export class AppModule {
   constructor(private iconSvc: SvgIconService) {
-    this.iconSvc.setErrorFill('purple');
+
+    this.iconSvc.setErrorTemplate(CUST_ERROR);
   }
 }
