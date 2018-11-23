15a22d79afb699126526e8912e358c6a4eb98e7d
Sanjeev Premi <spremi@ymail.com>
2018-11-24 00:41:50 +0530

    feat: lib: svg-icon: Define interface 'SvgGetOptions'
    The interface shall be used to pass options to get()
    method provided by the service.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- projects/spremi/svg-icon/src/lib/svg-get-options.ts
+++ projects/spremi/svg-icon/src/lib/svg-get-options.ts
@@ -1,2 +1,30 @@
+/**
+ * Size of the SVG image
+ */
+export interface SvgSize {
+  /**
+   * Width of the SVG image
+   */
+  width: number;
+
+  /**
+   * Height of the SVG image
+   */
+  height: number;
+}
+
+/**
+ * Option to get the SVG image.
+ * Scaling factor, if defined, takes precedence over size.
+ */
 export interface SvgGetOptions {
+  /**
+   * Size of the image, if defined.
+   */
+  size?: SvgSize;
+
+  /**
+   * Scaling factor of the image, if defined.
+   */
+  scale?: number;
 }
